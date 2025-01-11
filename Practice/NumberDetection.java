677f9f2b171ee3da8e2c2844100589ac5bb612e9
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2025-01-11 19:42:52 +0500

    My practice program

---
+++ Practice/NumberDetection.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class NumberDetection{
+public static void main(String[] args){
+System.out.println("Enter your number:");
+Scanner sc = new Scanner(System.in);
+System.out.println(sc.hasNextInt());
+
+}
+}
---
+++ Practice/WriteEquation.java
@@ -0,0 +1,9 @@
+public class WriteEquation{
+    public static void main(String [] args){
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int k = (b*b-4*a*c)/(2*a);
+        System.out.println(k);
+    }
+}
