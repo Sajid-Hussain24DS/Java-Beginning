8a6a6741f1068d00162b02a604eabcfc557daa6d
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2025-03-05 21:33:52 +0500

    add

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Calculator{
+    public static void main(System[]args){
+        Scanner Scanner = new Scanner(System.in);
+        int add;
+        int a ;
+        int b ;
+        add = a+b;
+        System.out.println(ad);
+  
+
+    }
+}
