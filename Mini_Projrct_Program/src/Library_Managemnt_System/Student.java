08c0ead11d26ffc59fb10e0ecedcb504611954eb
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2025-03-20 16:11:53 +0500

    add

---
+++ Mini_Projrct_Program/src/Library_Managemnt_System/Student.java
@@ -0,0 +1,21 @@
+package Library_Managemnt_System;
+
+public class Student {
+
+    String studentName;
+    int studentId;
+    String studentRollNumber;
+    String studentIsbian;
+
+    public Student(String studentName,int studentId, String studentRollNumber,String  studentIsbian) {
+        this.studentName = studentName;
+        this.studentId = studentId;
+        this.studentRollNumber = studentRollNumber;
+        this.studentIsbian = studentIsbian;
+    }
+    public void showStudentData() {
+        System.out.println(studentName + "     " + studentId + "     " + studentRollNumber + "     " + studentIsbian+"   ");
+    }
+
+
+}
