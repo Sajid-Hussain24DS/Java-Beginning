86d86004d45d90446cbae7aedb5343fd0a4359e2
Sajid-Hussain <lasharisajidhussain9@gmail.com>
2025-03-21 12:37:34 +0500

    cut it

--- Mini_Projrct_Program/src/Student_Management_System/Test.java
+++
@@ -1,9 +0,0 @@
-package Student_Management_System;
-
-public class Test {
-
-    public static void main(String[] args) {
-        StudentManagement studentsManagementSystem = new StudentManagement();
-        studentsManagementSystem.showMenu();
-    }
-}
